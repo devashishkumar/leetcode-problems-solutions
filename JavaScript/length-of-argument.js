bf0c975e3a2907885c4aa9022441f4450d394ece
Ashish <dev.ashishkumar0512@gmail.com>
2024-01-26 11:40:17 +0530

    Write a function argumentsLength that returns the count of arguments passed to it

---
+++ JavaScript/length-of-argument.js
@@ -0,0 +1,13 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    if (args.length > 0 && args.length <= 100) {
+        return args.length;
+    } else {
+        throw new Error("Array Length Should be Greater than 0 and less than or equal to 100");
+    }
+};
+argumentsLength({}, null, 3);
+
