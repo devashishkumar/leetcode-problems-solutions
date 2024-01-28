0dcfb2267a7f6bb26d4b9832effc7b2ee0af3099
Ashish <dev.ashishkumar0512@gmail.com>
2024-01-28 11:40:23 +0530

    join two arrays by id

---
+++ JavaScript/join-two-arrays-by-id.js
@@ -0,0 +1,22 @@
+// Join Two arrays by id
+
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function (arr1, arr2) {
+    return Object.values([...arr1, ...arr2].reduce((d, a) => (d[a.id] = {...d[a.id], ...a}) && d, {}))
+
+};
+
+join(
+  [
+    { id: 1, x: 2, y: 3 },
+    { id: 2, x: 3, y: 6 },
+  ],
+  [
+    { id: 2, x: 10, y: 20 },
+    { id: 3, x: 0, y: 0 },
+  ]
+);
